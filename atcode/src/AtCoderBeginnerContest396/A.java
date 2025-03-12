eec5ae88aaae89ecc865e6359ea7abac2bd87c92
dellgreen <fousenh2@gmail.com>
2025-03-12 17:41:46 +0900

    create Java project

---
+++ atcode/src/AtCoderBeginnerContest396/A.java
@@ -0,0 +1,12 @@
+package AtCoderBeginnerContest396;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class A {
+    
+    public void test(){
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+    }
+}
