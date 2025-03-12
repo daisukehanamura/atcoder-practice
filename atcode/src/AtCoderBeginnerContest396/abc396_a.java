0bdc0e0d3c4b225bac2b6ed6ce68c6683976195e
dellgreen <fousenh2@gmail.com>
2025-03-12 18:11:01 +0900

    fix

---
+++ atcode/src/AtCoderBeginnerContest396/abc396_a.java
@@ -0,0 +1,8 @@
+package AtCoderBeginnerContest396;
+
+public class abc396_a {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ atcode/src/AtCoderBeginnerContest396/abc396_c.java
@@ -0,0 +1,21 @@
+package AtCoderBeginnerContest396;
+
+import java.util.Scanner;
+
+public class abc396_c {
+        public static void main(String[] args) throws Exception {
+            Scanner sc = new Scanner(System.in);
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            int [] B = new int[N];
+            int [] W = new int[M];
+            
+            for(int i=0; i< N; i++){
+                B[i] = sc.nextInt();
+            }
+
+            for(int i=0; i< M; i++){
+                W[i] = sc.nextInt();
+            }
+    }
+}
